f95384039dee857d6ce6044e630a1f8842292e09
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-05-21 10:06:13 +0100

    Configuration de la connection JDBC

--- src/main/java/com/util/DbConnection.java
+++ src/main/java/com/util/DbConnection.java
@@ -1,4 +1,23 @@
 package com.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DbConnection {
+
+    private static final String URL = "jdbc:postgresql://localhost:5432/GestionConstruction";
+    private static final String USERNAME = "postgres";
+    private static final String PASSWORD = "admin";
+
+    public static Connection getConnection() throws SQLException {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            System.err.println("Error connecting to the database: " + e.getMessage());
+            throw e;
+        }
+        return connection;
+    }
 }
