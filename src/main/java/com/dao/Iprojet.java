455f8109be190233fbf096e02f6cd9af6239eb23
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-05-22 10:27:38 +0100

    Création de l'interface Iprojet

--- src/main/java/com/dao/Iprojet.java
+++ src/main/java/com/dao/Iprojet.java
@@ -1,4 +1,17 @@
 package com.dao;
 
+import com.model.Projet;
+import java.sql.SQLException;
+import java.util.List;
+
+
 public interface Iprojet {
+    void ajouterProjet(Projet projet) throws SQLException;
+    List<Projet> afficherListeProjets() throws SQLException;
+    void modifierProjet(Projet projet) throws SQLException;
+    void supprimerProjet(int idProjet) throws SQLException;
 }
+
+
+
+
