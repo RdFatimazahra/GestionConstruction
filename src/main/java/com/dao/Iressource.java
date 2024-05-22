f931e112fdd76b29a616fc6fe161487274cdf483
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-05-22 10:29:58 +0100

    Création de l'interface Iressource

--- src/main/java/com/dao/Iressource.java
+++ src/main/java/com/dao/Iressource.java
@@ -1,5 +1,13 @@
 package com.dao;
 
+import com.model.Ressource;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface Iressource {
+    void ajouterRessource(Ressource ressource) throws SQLException;
+    List<Ressource> afficherListeRessources() throws SQLException;
+    void modifierRessource(Ressource ressource) throws SQLException;
+    void supprimerRessource(int idRessource) throws SQLException;
 
 }
