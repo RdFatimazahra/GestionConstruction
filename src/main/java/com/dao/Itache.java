f5cdc337a3ee28cd7602d95bf08e6232ac5b00b9
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-05-22 10:28:56 +0100

    Création de l'interface Itache

--- src/main/java/com/dao/Itache.java
+++ src/main/java/com/dao/Itache.java
@@ -1,4 +1,12 @@
 package com.dao;
 
+import com.model.Tache;
+import java.sql.SQLException;
+import java.util.List;
+
 public interface Itache {
+    void ajouterTache(Tache tache) throws SQLException;
+    List<Tache> afficherListeTaches() throws SQLException;
+    void modifierTache(Tache tache) throws SQLException;
+    void supprimerTache(int idTache) throws SQLException;
 }
