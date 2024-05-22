7d4ec3082c8f38922179e9fbbdeff1aaa5e8afdf
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-05-22 09:53:12 +0100

    Création classe beans Projet

--- src/main/java/com/model/Projet.java
+++ src/main/java/com/model/Projet.java
@@ -1,4 +1,85 @@
 package com.model;
 
+import java.util.Date;
+
 public class Projet {
+    private int idProjet;
+    private String nomProjet;
+    private String description;
+    private Date dateDebut;
+    private Date dateFin;
+    private double budget;
+
+    // Constructeurs
+    public Projet() {}
+
+    public Projet(String nomProjet, String description, Date dateDebut, Date dateFin, double budget) {
+        this.nomProjet = nomProjet;
+        this.description = description;
+        this.dateDebut = dateDebut;
+        this.dateFin = dateFin;
+        this.budget = budget;
+    }
+
+    // Getters et Setters
+    public int getIdProjet() {
+        return idProjet;
+    }
+
+    public void setIdProjet(int idProjet) {
+        this.idProjet = idProjet;
+    }
+
+    public String getNomProjet() {
+        return nomProjet;
+    }
+
+    public void setNomProjet(String nomProjet) {
+        this.nomProjet = nomProjet;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getDateDebut() {
+        return dateDebut;
+    }
+
+    public void setDateDebut(Date dateDebut) {
+        this.dateDebut = dateDebut;
+    }
+
+    public Date getDateFin() {
+        return dateFin;
+    }
+
+    public void setDateFin(Date dateFin) {
+        this.dateFin = dateFin;
+    }
+
+    public double getBudget() {
+        return budget;
+    }
+
+    public void setBudget(double budget) {
+        this.budget = budget;
+    }
+
+    // Méthode toString
+    @Override
+    public String toString() {
+        return "Projet{" +
+                "idProjet=" + idProjet +
+                ", nomProjet='" + nomProjet + '\'' +
+                ", description='" + description + '\'' +
+                ", dateDebut=" + dateDebut +
+                ", dateFin=" + dateFin +
+                ", budget=" + budget +
+                '}';
+    }
 }
